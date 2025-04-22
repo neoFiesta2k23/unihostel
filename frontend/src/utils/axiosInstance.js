a3bf066d83d89152974d568bf6d82ddab4474537
neoFiesta2k23 <165475922+neoFiesta2k23@users.noreply.github.com>
2025-04-23 03:12:54 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api/v1'
-    // baseURL: 'http://192.168.239.3:5000/api/v1'
-    // baseURL: 'http://192.168.137.194:5000/api/v1'
+    baseURL: 'https://unihostel-backend.onrender.com/api/v1'
+    // baseURL: 'http://127.0.0.1:5000/api/v1'
 });
 
 export default axiosInstance
