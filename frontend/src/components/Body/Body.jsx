404b34802d136880cadc3d83c1a99d210d6551db
neoFiesta2k23 <165475922+neoFiesta2k23@users.noreply.github.com>
2025-04-23 03:38:40 +0530

    same same same

--- frontend/src/components/Body/Body.jsx
+++ frontend/src/components/Body/Body.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../header/Header";
 import Footer from "../Footer/Footer";
 import Body1 from "./body1/Body1";
-import Body2 from "./body2/body2";
+import Body2 from "./body2/Body2";
 import Gallery from "./Gallery/Gallery";
 
 
